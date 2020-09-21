c755b437ad87f8fad53cecbb1a3b76f7a7c725ec
boyloe <boyloe@gmail.com>
2020-09-21 08:55:53 -0600

    Solve a Very Large Sum

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function simpleArraySum(ar) {
 }
 
 //09/21/2020 'Compare the Triplets'
-function compareTheTriplets(aArray, bArray){
+function compareTriplets(aArray, bArray){
     let a = 0, b = 0
     for(let i = 0; i < aArray.length; i ++){
         if( aArray[i] < bArray[i] ) 
@@ -18,3 +18,9 @@ function compareTheTriplets(aArray, bArray){
     }
     return [a,b]
 }
+
+//09/21/2020 'A Very Large Sum'
+function veryLargeSum(ar) {
+    return ar.reduce((acc, currentValue) => acc + currentValue,0)
+ }
+ 
