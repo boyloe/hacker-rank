0c581ba882a09bdbe6fd31579eff08f49b3be09a
boyloe <boyloe@gmail.com>
2020-09-21 08:37:21 -0600

    Solve Simple Array Sum

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 //09/21/2020 'Simple Array Sum'
+function simpleArraySum(ar) {
+   return ar.reduce((acc, currentValue) => acc + currentValue,0)
+}
+let ar = [1,2,3,4,10,11]
+console.log(simpleArraySum(ar))
