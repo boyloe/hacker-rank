e0b80b271307c929f60a4210d49c1731b136dc7c
boyloe <boyloe@gmail.com>
2020-09-22 08:48:14 -0600

    Solve Diagonal Difference

--- index.js
+++ index.js
@@ -22,5 +22,17 @@ function compareTriplets(aArray, bArray){
 //09/21/2020 'A Very Large Sum'
 function veryLargeSum(ar) {
     return ar.reduce((acc, currentValue) => acc + currentValue,0)
- }
- 
+}
+
+09//22/2020 'Diagonal Difference'
+function diagonalDifference(array) {
+    let diag1 = 0
+    let diag2 = 0
+    let length = array[0].length
+    for ( let i = 0; i < length; i ++ ) {
+        diag1 += array[i][i]
+        diag2 += array[i][length - i - 1]
+    }
+    return Math.abs( diag1 - diag2 )
+}
+
